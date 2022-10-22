bb791596b38ea6e95434d673da5ac9152fb326fc
Nicolas Brosan <nicolasbrosan@hotmail.com>
2022-10-22 18:34:19 -0300

    modificaciones en las clases Usuario y Cliente

--- Domain/Negocio/Cliente.cs
+++ Domain/Negocio/Cliente.cs
@@ -11,6 +11,8 @@ namespace Domain.Negocio
         public string Apellido { get; set; }
         public int Telefono { get; set; }
         public string Direccion { get; set; }
+        public string Localidad { get; set; }
+        public string Provincia { get; set; }
         public int Tarjeta { get; set; }
     }
 }
