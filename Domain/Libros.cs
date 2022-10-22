95429aea62e5103eb20001fff8506bbf3f256bda
Nicolas Brosan <nicolasbrosan@hotmail.com>
2022-10-22 17:31:30 -0300

    Se crea una clase

---
+++ Domain/Libros.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Domain
+{
+    public class Libros
+    {
+        public string Nombre { get; set; }
+        public string Autor { get; set; }
+        public string Editorial { get; set; }
+        public decimal Precio { get; set; }
+    }
+}
