0e18c5a756bcbe10d00cb46deb107dedbfa6459a
Nicolas Brosan <nicolasbrosan@hotmail.com>
2022-10-22 18:32:33 -0300

    modificaciones en las clases Usuario y Cliente

--- Domain/Autenticacion/Usuario.cs
+++ Domain/Autenticacion/Usuario.cs
@@ -2,14 +2,10 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace Domain
+namespace Domain.Autenticacion
 {
     public class Usuario
     {
-        public int Id { get; set; }
-        public string Nombre { get; set; }
-        public string Apellido { get; set; }
-        public string NombreUsuario { get; set; }
         public string Password { get; set; }
         public string Mail { get; set; }
 
---
+++ Domain/Negocio/Cliente.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Domain.Negocio
+{
+    public class Cliente
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public int Telefono { get; set; }
+        public string Direccion { get; set; }
+        public int Tarjeta { get; set; }
+    }
+}
