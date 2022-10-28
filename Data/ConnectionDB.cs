5aecd2817cfdd4bcbcd17ea7ba344ff3cb969cd9
Nicolas Brosan <nicolasbrosan@hotmail.com>
2022-10-28 09:55:14 -0300

    Se agrega LibroHandler

---
+++ Data/ConnectionDB.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Configuration;
+using System.Data.SqlClient;
+
+namespace Data
+{
+    public class ConnectionDB
+    {
+        private string connection;
+        public string CConnection(string DB)
+        {
+            connection = ConfigurationManager.ConnectionStrings[DB].ConnectionString;
+            return connection;
+        }
+        
+    }
+}
--- Data/LibroHandler.cs
+++ Data/LibroHandler.cs
@@ -1,12 +1,27 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using Domain.Negocio;
+using System;
 using System.Data.SqlClient;
-using System.Text;
 
 namespace Data
 {
     public class LibroHandler
     {
-        
+        ConnectionDB cnn = new ConnectionDB();
+        public void CreateLibro(Libro libro)
+        {            
+            using (SqlConnection conexion = new SqlConnection(cnn.CConnection("myConnection")))
+            {
+                try
+                {
+                    string queryLibro = "INSERT INTO Libro (Nombre, Autor, Editorial, Sinopsis, Precio, Stock, IdUsuario) VALUES (@nombre, @autor, @editorial, @sinopsis, @precio, @stock, @idUsuario)";
+
+                    SqlParameter nombre = new SqlParameter("Nombre", System.Data.SqlDbType.VarChar) { Value = libro.Nombre };
+                }
+                catch (Exception)
+                {
+                                           
+                }
+            }
+        }
     }
 }
