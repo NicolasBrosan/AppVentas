dbe9869c9a4dfdfa5c427d7f62f086ec0ab16229
Nicolas Brosan <nicolasbrosan@hotmail.com>
2022-10-28 12:35:58 -0300

    actualizo LibroHandler

--- Data/LibroHandler.cs
+++ Data/LibroHandler.cs
@@ -1,14 +1,15 @@
 ﻿using Domain.Negocio;
 using System;
+using System.Collections.Generic;
 using System.Data.SqlClient;
 
 namespace Data
 {
     public class LibroHandler
-    {
-        ConnectionDB cnn = new ConnectionDB();
-        public void CreateLibro(Libro libro)
-        {            
+    {        
+        public static void CreateLibro(Libro libro)
+        {
+            ConnectionDB cnn = new ConnectionDB();
             using (SqlConnection conexion = new SqlConnection(cnn.CConnection("myConnection")))
             {
                 try
@@ -33,14 +34,129 @@ namespace Data
                         cmd.Parameters.Add(precio);
                         cmd.Parameters.Add(stock);
                         cmd.Parameters.Add(idUsuario);
+
+                        int numberOfRows = cmd.ExecuteNonQuery();
                     }
                     conexion.Close();
                 }
                 catch (Exception)
                 {
-                    Console.WriteLine("No se pudo realizar la operacion");                          
+                    Console.WriteLine("No se pudo insertar los datos");                          
                 }
             }
+        }
+
+        public static void UpdateLibro (Libro libro)
+        {
+            ConnectionDB cnn = new ConnectionDB();
+            using (SqlConnection conexion = new SqlConnection(cnn.CConnection("myConnection")))
+            {
+                try
+                {
+                    string queryUpLibro = "UPDATE [DB_VentaLibros].[dbo].[Libro] SET Nombre = @nombre, Autor = @autor, Editorial = @editorial, Sinopsis = @sinopsis, Precio = @precio, Stock = @stock, IdUsuario = @idUsuario WHERE Id = @id";
+
+                    SqlParameter nombre = new SqlParameter("nombre", System.Data.SqlDbType.VarChar) { Value = libro.Nombre };
+                    SqlParameter autor = new SqlParameter("autor", System.Data.SqlDbType.VarChar) { Value = libro.Autor };
+                    SqlParameter editorial = new SqlParameter("editorial", System.Data.SqlDbType.VarChar) { Value = libro.Editorial };
+                    SqlParameter sinopsis = new SqlParameter("sinopsis", System.Data.SqlDbType.VarChar) { Value = libro.Sinopsis };
+                    SqlParameter precio = new SqlParameter("precio", System.Data.SqlDbType.Decimal) { Value = libro.Precio };
+                    SqlParameter stock = new SqlParameter("stock", System.Data.SqlDbType.Int) { Value = libro.Stock };
+                    SqlParameter idUsuario = new SqlParameter("idUsuario", System.Data.SqlDbType.Int) { Value = libro.IdUsuario };
+                    SqlParameter id = new SqlParameter("id", System.Data.SqlDbType.BigInt) { Value = libro.Id };
+
+                    conexion.Open();
+                    using(SqlCommand cmd= new SqlCommand(queryUpLibro, conexion))
+                    {
+                        cmd.Parameters.Add(nombre);
+                        cmd.Parameters.Add(autor);
+                        cmd.Parameters.Add(editorial);
+                        cmd.Parameters.Add(sinopsis);
+                        cmd.Parameters.Add(precio);
+                        cmd.Parameters.Add(stock);
+                        cmd.Parameters.Add(idUsuario);
+                        cmd.Parameters.Add(id);
+
+                        int NumberOfRows = cmd.ExecuteNonQuery();
+                    }
+                    conexion.Close();
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("No se pudo actualizar");                    
+                }
+            }
+        }
+        public static void DeleteLibro (int id)
+        {
+            ConnectionDB cnn = new ConnectionDB();
+            using (SqlConnection conexion = new SqlConnection(cnn.CConnection("myConnection")))
+            {
+                try
+                {
+                    List<string> Querys = new List<string>();
+
+                    var queryDeletePV = "DELETE ProductoVendido FROM Libro AS L INNER JOIN ProductoVendido AS PV on L.ID = PV.IDLIBRO WHERE L.ID = @id";
+                    Querys.Add(queryDeletePV);
+
+                    string queryDtLibro = "DELETE FROM Libro WHERE Id = @id";
+                    Querys.Add(queryDtLibro);
+
+                    conexion.Open();
+                    foreach(var query in Querys)
+                    {
+                        SqlParameter sqlParameter = new SqlParameter("id",      System.Data.SqlDbType.BigInt);
+                        sqlParameter.Value = id;
+
+                        using(SqlCommand cmd = new SqlCommand(query,                  conexion))
+                        {
+                            cmd.Parameters.Add(sqlParameter);
+                            int numberOfRows = cmd.ExecuteNonQuery();
+                        }
+                    }
+                    conexion.Close();
+                }
+                catch(Exception)
+                {
+                    Console.WriteLine("No se pudo eliminar lo solicitado");
+                }
+
+
+            }
+        }
+        public static List<Libro> GetLibros()
+        {
+            ConnectionDB cnn = new ConnectionDB();
+            List<Libro> libros = new List<Libro>();
+            using(SqlConnection conexion = new SqlConnection(cnn.CConnection("myConnection")))
+            {
+                try
+                {
+                    var queryGetLibros = "SELECT * FROM Libro";
+                    using(SqlCommand cmd = new SqlCommand(queryGetLibros, conexion))
+                    {
+                        conexion.Open();
+                        using(SqlDataReader reader = cmd.ExecuteReader())
+                        {
+                            if (reader.HasRows)
+                            {
+                                while (reader.Read())
+                                {
+                                    Libro libro = new Libro();
+                                    libro.Id = Convert.ToInt32(reader["Id"]);
+                                    libro.Nombre = reader["Nombre"].ToString();
+
+                                }
+                            }
+                        }
+                    }
+                }
+                catch (Exception)
+                {
+
+                    
+                }
+            }
+
         }
     }
 }
