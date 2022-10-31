e5e32442400bc7b48b666f436fcd48c3850ff512
Nicolas Brosan <nicolasbrosan@hotmail.com>
2022-10-31 07:57:47 -0300

    Se actualiza VentaHandler

--- Data/VentaHandler.cs
+++ Data/VentaHandler.cs
@@ -48,13 +48,47 @@ namespace Data
                             }
                         }
                     }
+
+                    //Se recorre la lista de Libro para insertar la venta en ProductoVendido
+                    foreach(var libroVendido in libro)
+                    {
+                        var queryLibroVendido = "INSERT INTO ProductoVendido (IdLibro, Stock, IdVenta) VALUES (@idLibro, @stock, @idVenta)";
+
+                        SqlParameter idLibro = new SqlParameter("IdLibro", System.Data.SqlDbType.BigInt) { Value = libroVendido.Id };
+                        SqlParameter stock = new SqlParameter("Stock", System.Data.SqlDbType.BigInt) { Value = libroVendido.Stock };
+                        SqlParameter idVenta = new SqlParameter("IdVenta", System.Data.SqlDbType.BigInt) { Value = idVentas };
+
+                        using(SqlCommand cmd = new SqlCommand(queryLibroVendido, conexion))
+                        {
+                            cmd.Parameters.Add(idLibro);
+                            cmd.Parameters.Add(stock);
+                            cmd.Parameters.Add(idVenta);
+
+                            int numberOfRows = cmd.ExecuteNonQuery();
+                        }
+
+                        //Actualizo el stock de Libro
+                        var queryUpdateStock = "UPDATE Libro SET Stock (Stock - @Stock) WHERE Id = @id";
+
+                        SqlParameter id = new SqlParameter("Id", System.Data.SqlDbType.BigInt) { Value = libroVendido.Id };
+                        SqlParameter stockVendido = new SqlParameter("Stock", System.Data.SqlDbType.BigInt) { Value = libroVendido.Stock };
+
+                        using(SqlCommand cmd = new SqlCommand(queryUpdateStock, conexion))
+                        {
+                            cmd.Parameters.Add(id);
+                            cmd.Parameters.Add(stockVendido);
+
+                            int numberOfRows = cmd.ExecuteNonQuery();
+                        }
+                    }
+
+                    conexion.Close();
                 }
 
             }
             catch (Exception)
             {
-
-                
+                Console.WriteLine("No se pudo realizar la operacion");
             }
         }
     }
