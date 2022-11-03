2315864ede72c2011feadbf6dd3324de406538b6
Nicolas Brosan <nicolasbrosan@hotmail.com>
2022-11-03 19:55:11 -0300

    registros de Usuario

--- Data/UsuarioHandler.cs
+++ Data/UsuarioHandler.cs
@@ -5,28 +5,47 @@ using Data.Interface;
 using System.Data.SqlClient;
 using System.Configuration;
 using Domain.Autenticacion;
+using System.Data;
 
 namespace Data
 {
     public class UsuarioHandler
     {
         ConnectionDB cnn = new ConnectionDB();
-
-        public Usuario InsertForLogin () //Como podria enlazar la info de Cliente con Usuario para hacer el Login??
+        public void RegistrarUsuario (Usuario usuario)
         {
-            try
+            using(SqlConnection conexion = new SqlConnection(cnn.CConnection("myConnection")))
             {
-                using (SqlConnection conexion = new SqlConnection(cnn.CConnection("myConnection")))
+                try
                 {
-                    var queryData = "";  //¡¡¿?!!
+                    var queryInsertUsuario = "INSERT INTO Usuario (Password, Mail, IdCliente) VALUES (@password, @mail, @idCliente)";
+                    var password = new SqlParameter("Password", System.Data.SqlDbType.VarChar) { Value = usuario.Password };
+                    var mail = new SqlParameter("Mail", SqlDbType.VarChar) { Value = usuario.Mail };
+                    var idCliente = new SqlParameter("IdCliente", System.Data.SqlDbType.Int) { Value = usuario.IdCliente };
+
+                    conexion.Open();
+
+                    using (SqlCommand cmd = new SqlCommand(queryInsertUsuario, conexion))
+                    {
+                        cmd.Parameters.Add(password);
+                        cmd.Parameters.Add(mail);
+                        cmd.Parameters.Add(idCliente);
+
+                        int numberOfRows = cmd.ExecuteNonQuery();
+                    }
+
+                    conexion.Close();
+
                 }
+                catch (Exception ex)
+                {
 
+                    Console.WriteLine(ex.Message);
+                }
             }
-            catch (Exception ex)
-            {
-
-                Console.WriteLine(ex.Message);
-            }
+            
         } 
+
+
     }
 }
